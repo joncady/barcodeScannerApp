28b76de338e935f715ac86693a4060b551146e95
Jonathan Cady <joncady@uw.edu>
2019-02-16 22:56:58 -0800

    Updates

---
+++ precache-manifest.99cc41af9bdabbe4aaef4dde4399a62e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "854b0b086921bb6d934f",
+    "url": "/barcodeScannerApp/static/js/runtime~main.854b0b08.js"
+  },
+  {
+    "revision": "1712b282648c39ae27e0",
+    "url": "/barcodeScannerApp/static/js/main.1712b282.chunk.js"
+  },
+  {
+    "revision": "c74fb574416ac088f3cd",
+    "url": "/barcodeScannerApp/static/js/2.c74fb574.chunk.js"
+  },
+  {
+    "revision": "1712b282648c39ae27e0",
+    "url": "/barcodeScannerApp/static/css/main.9cedd865.chunk.css"
+  },
+  {
+    "revision": "c74fb574416ac088f3cd",
+    "url": "/barcodeScannerApp/static/css/2.5face483.chunk.css"
+  },
+  {
+    "revision": "3416a3e80f4ef4cb4fef32a958dc025d",
+    "url": "/barcodeScannerApp/index.html"
+  }
+];
